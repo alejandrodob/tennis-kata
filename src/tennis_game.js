b8aaa3590b01931ed5a1aa1ba7602f4818a72d07
Alejandro Dominguez <alejandro@Alejandros-MacBook-Pro.local>
2014-01-07 17:33:46 +0100

    initial commit

---
+++ src/tennis_game.js
@@ -0,0 +1,48 @@
+var TennisGame = function(player1, player2) {
+  var SCORES = ['Love', 'Fifteen', 'Thirteen', 'Fourteen'];
+  var self = {};
+
+  self.score = [0, 0];
+
+  var tied = function() {
+    return self.score[0] === self.score[1];
+  };
+
+  var evaluateTied = function() {
+    if (self.score[0] >= 3) {
+      return 'Deuce';
+    }
+    return  SCORES[self.score[1]] + ' all';
+  };
+
+  var enoughAdvantage  = function() {
+    return ((self.score[0] - self.score[1]) > 2);
+  };
+
+  var winner = function() {
+    return (self.score[0] > 3 || self.score[1] > 3) && enoughAdvantage();
+  };
+
+  var playerInAdvantage = function() {
+    return self.score[0] > self.score[1] ? player1 : player2;
+  };
+
+  self.getScore = function() {
+
+    if (tied()) {
+      return evaluateTied();
+    } else {
+      if (winner()){
+        return player1 + ' wins!!';
+      } else if (self.score[0] > 3 || self.score[1] > 3 ) {
+        return 'Advantage ' + playerInAdvantage();
+      }
+      return SCORES[self.score[0]] + ' ' + SCORES[self.score[1]];
+    }
+  };
+
+  return self;
+};
+
+
+
---
+++ test/spec/tennis_game_spec.js
@@ -0,0 +1,79 @@
+context = describe;
+
+
+describe("Tennis Game", function() {
+  var game;
+
+  beforeEach(function() {
+    game = TennisGame('player 1', 'player 2');
+  });
+
+  it("returns 'Love all' at start up", function() {
+    expect(game.getScore()).toBe('Love all');
+  });
+
+  it("returns 'Fifteen love' after player 1 scores", function(){
+    game.nextPlay = function(){
+      game.score[0] += 1;
+    }();
+
+    expect(game.getScore()).toBe('Fifteen Love');
+  });
+
+  it("return Love fifteen after player 2 scores", function(){
+    game.nextPlay = function(){
+      game.score[1] += 1;
+    }();
+
+    expect(game.getScore()).toBe('Love Fifteen');
+  });
+
+  it("returns 'Fifteen all' after both players score", function() {
+    game.nextPlay = function() {
+      game.score = [1, 1];
+    }();
+
+    expect(game.getScore()).toBe('Fifteen all');
+  });
+
+  it("returns 'Deuce' if players are tied at '40", function() {
+    game.nextPlay = function() {
+      game.score = [3, 3];
+    }();
+
+    expect(game.getScore()).toBe('Deuce');
+  });
+
+  it("returns 'Advantage for player 1 scores after 'Deuce'", function() {
+    game.nextPlay = function() {
+      game.score = [4, 3];
+    }();
+
+    expect(game.getScore()).toBe('Advantage player 1');
+  });
+
+  it("returns 'Advantage for player 2 scores after 'Deuce'", function() {
+    game.nextPlay = function() {
+      game.score = [3, 4];
+    }();
+
+    expect(game.getScore()).toBe('Advantage player 2');
+  });
+
+  it("returns 'Player 1 wins!!' when player 1 scores the championship point", function() {
+    game.nextPlay = function() {
+      game.score = [5, 3];
+    }();
+
+    expect(game.getScore()).toBe('player 1 wins!!');
+  });
+
+});
+
+
+
+
+
+
+
+
